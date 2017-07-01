54cbb104123951f6c1ab6d1b161a50cef981a2b6
Sam Gregory <samuel@digital-detox.co.uk>
2017-07-01 16:37:36 +0100

    added css files

---
+++ themes/twofold-wp-child/inc/templates/blog/blog-header.php
@@ -0,0 +1,8 @@
+
+<?php if ($blog_header && is_home()) { ?>
+	<div class="row expanded">
+		<div class="small-12 columns">
+			<?php echo do_shortcode($blog_header); ?>
+		</div>
+	</div>
+<?php } ?>
